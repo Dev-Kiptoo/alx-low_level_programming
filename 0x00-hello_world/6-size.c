59ca570cef49163a64729b96a31fc570b1821470
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-06-15 10:16:01 -0700

    Intel!!

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,8 @@ int main(void)
 {
 	int i = sizeof(char);
 	int j = sizeof(int);
-	int k = sizeof(long int);
-	int l = sizeof(float);
-	
-	printf("%d%d%d%d\n", i, j, k, l);
+	k = "sizeof(lonint)";
+	l = 
+	printf
 	return (0);
 }
-
