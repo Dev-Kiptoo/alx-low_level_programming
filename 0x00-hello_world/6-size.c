bc05e5777577a596d91f308db7c504ecde1286db
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-06-15 14:09:21 -0700

    Size!!

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char:%lu byte(s)\n", (unsigned long) sizeof(a));
 	printf("Size of an int:%lu bytes(s)\n", (unsigned long) sizeof(a));
-	printf("Size of a long int: %lu bytes(s)\n", (unsigned long) sizeof(k));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long) sizeof(w));
-	printf("Size of a float: %lu bytes(s)", (unsigned long) sizeof(y));
+	printf("Size of a long int:%lu bytes(s)\n", (unsigned long) sizeof(k));
+	printf("Size of a long long int:%lu bytes(s)\n", (unsigned long) sizeof(w));
+	printf("Size of a float:%lu bytes(s)\n", (unsigned long) sizeof(y));
 	return (0);
 }
