304d74e7c7cd4a424f6d547aabc4789e3ba7a8f7
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-06-16 06:35:03 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return : Always 0 (Success)
+ */
+int main(void)
+	/* returns my value*/
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
+
