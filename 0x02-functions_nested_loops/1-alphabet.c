233bdd402594d828c65a408f7b4cd06daa217021
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-06-20 10:56:48 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * main - check code
+ * print_alphabet - check code
  *
  * Return: Always 0 (Successful)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
 		_putchar(a);
 	_putchar('\n');
-	return (0);
 }
