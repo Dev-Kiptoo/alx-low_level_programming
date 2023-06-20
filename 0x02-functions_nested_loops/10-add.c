26212f204a9c2f21e9ea8e63697a711b60a34d7e
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-06-20 09:32:48 -0700

    Addition !!

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - addition of two numbers
+ *
+ * @a: numbers to  be added
+ * @b: value to be added to a
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
