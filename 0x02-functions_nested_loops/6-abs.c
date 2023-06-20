84d2742c980cc0ea4f3e1fdc82110314c3511021
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-06-20 06:33:30 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute
+ * @int: the integer
+ * Return: absolute of the int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
