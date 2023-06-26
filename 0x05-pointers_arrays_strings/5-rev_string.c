9a752503a5ff7f6ca2b1e1758abd3c949b79b0bd
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-06-26 22:39:16 +0300

    arrays and pointers resolved

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,24 @@
 #include "main.h"
+
 /**
- * print_rev - prints the reverse of a character
- * @s: the string to be returned
- * Return: the reverse
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
  */
+
 void rev_string(char *s)
 {
-	int length = 0;
+	char rev = s[0];
+	int counter = 0;
 	int i;
 
-	while (*s != '\0')
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
 	{
-		length ++;
-		s++;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
-	s--;
-	for (i = length; i >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
 }
