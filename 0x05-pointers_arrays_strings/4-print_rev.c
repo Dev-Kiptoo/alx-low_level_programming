bd405620da1e70721623a9c30d9d21ed4399be30
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-06-26 14:11:42 -0700

    Quiz 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,25 @@
 #include "main.h"
 /**
- * print_rev - prints the reverse of a character
- * @s: the string to be returned
- * Return: the reverse
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
  */
 void print_rev(char *s)
 {
-	int length = 0;
-	int i;
+	int longi = 0;
+	int o;
 
 	while (*s != '\0')
 	{
-		length++;
+		longi++;
 		s++;
 	}
 	s--;
-	for (i = length; i >= 0; i--)
+	for (o = longi; o > 0; o--)
 	{
 		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
 }
