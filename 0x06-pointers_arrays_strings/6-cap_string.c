1b433e6f417e128e4cb25197e3fd9295f5d72097
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-06-29 10:55:18 -0700

    added

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * *cap_string - capitalize all words of a string
- * 
+ * *cap_string - capitalize all words of a string 
  * Return: arr
 */
 char *cap_string(char *arr)
 {
-    bool capitalizeNext = true;
-    while (*arr)
-    {
-        if (isspace(*arr))
-        {
-            capitalizeNext = true;
-        }
-        else if (capitalizeNext)
-        {
-            *arr = toupper(*arr);
-            capitalizeNext = false;
-        }
-        else
-        {
-            *arr = tolower(*arr);
-        }
-        arr++;
-    }
-    return (arr);
+	bool capitalizeNext = true;
+
+	while (*arr)
+	{
+		if (isspace(*arr))
+		{
+			capitalizeNext = true;
+		}
+		else if (capitalizeNext)
+		{
+			*arr = toupper(*arr);
+			capitalizeNext = false;
+		}
+		else
+		{
+			*arr = tolower(*arr);
+		}
+		arr++;
+	}
+	return (arr);
 }
