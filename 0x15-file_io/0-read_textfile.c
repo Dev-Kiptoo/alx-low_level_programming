d92d3212ae4b4e2925c387d0f97d24d5b7eb424b
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-08-07 07:16:06 -0700

    file input and output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /*
-* read_textfile - reads a text file and prints it to the POSIX standard output
-* @filename: it the filename to be opened
-* @letters: it is the letter to be opened
-*
-* Return: returns the Writtenbytes
-*/
+ * read_textfile - a function  that prints in the POSIX stdout
+ * @filename: file to be opened
+ * @letters: the file that returns the actual number of the letters in the function
+ * Return: w
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        if (filename == NULL)
-        {
-                return (0);
-        }
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-        FILE *file = fopen(filename, "r");
-        if (file == NULL)
-                return (0);
-        char *mem = malloc(sizeof(char) * (letters + 1));
-        if (mem == NULL)
-        {
-                fclose(file);
-                return (0);
-        }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-        ssize_t read = fread(mem, sizeof(char), letters, file);
-        if (read <= 0)
-        {
-                fclose(file);
-                free(mem);
-                return (0);
-        }
-
-        ssize_t write = fwrite(mem, sizeof(char), read, stdout);
-        if (write < read)
-        {
-                fclose(file);
-                free(mem);
-                return (0);
-        }
-
-        free(mem);
-        fclose(file);
-        return (read);
+	free(buf);
+	close(fd);
+	return (w);
 }
